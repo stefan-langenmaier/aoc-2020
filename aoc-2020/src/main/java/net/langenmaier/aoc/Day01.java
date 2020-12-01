b27366607f59a775b48a38fb979821f7a3c160af
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-12-01 18:51:36 +0100

    solution Day01 - 01

--- aoc-2020/src/main/java/net/langenmaier/aoc/Day01.java
+++ aoc-2020/src/main/java/net/langenmaier/aoc/Day01.java
@@ -1,13 +1,29 @@
 package net.langenmaier.aoc;
 
-/**
- * Hello world!
- *
- */
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Day01 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        InputStream inputStream = Day01.class.getClassLoader().getResourceAsStream("aoc-input-01.txt");
+        Scanner inputScanner = new Scanner(inputStream);
+
+        HashSet<Integer> list = new HashSet<Integer>();
+        
+        while (inputScanner.hasNext()) {
+            list.add(inputScanner.nextInt());
+        }
+        inputScanner.close();
+
+        for (Integer element : list) {
+            Integer inverse = 2020 - element;
+            if (list.contains(inverse)) {
+                System.out.println(inverse * element);
+                System.exit(0);
+            }
+        }
     }
 }
