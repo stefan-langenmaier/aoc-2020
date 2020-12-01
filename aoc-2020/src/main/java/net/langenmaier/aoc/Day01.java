e8940c66e13e9ba15d99eec5ba34103dab8fee8d
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-12-01 18:27:05 +0100

    add Day 01

---
+++ aoc-2020/src/main/java/net/langenmaier/aoc/Day01.java
@@ -0,0 +1,13 @@
+package net.langenmaier.aoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class Day01 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ aoc-2020/src/test/java/net/langenmaier/aoc/Day01Test.java
@@ -0,0 +1,20 @@
+package net.langenmaier.aoc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Day01Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
